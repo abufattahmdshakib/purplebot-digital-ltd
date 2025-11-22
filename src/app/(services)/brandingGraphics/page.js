998e37459394e7a94792398b8a201e4d097c955b
abufattahmdshakib <abufattahmdshakib21@gmail.com>
2025-11-22 12:36:02 +0600

    fix scrolling error

--- src/app/(services)/brandingGraphics/page.js
+++ src/app/(services)/brandingGraphics/page.js
@@ -13,7 +13,7 @@ function BrandingGraphics() {
       <div className='pt-4 sm:pt-6'>
         <div className="flex flex-col justify-between">
           <div className='flex-1'>
-            <div className=' pt-12 relative z-0'>
+            <div className=' pt-12 relative z-0 overflow-x-hidden'>
               <Branding />
               <LogoandBrand />
               <PrintMaterial />
